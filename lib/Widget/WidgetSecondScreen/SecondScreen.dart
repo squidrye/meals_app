cef01910c9d3c18a673a33c8a1cca6405405db9b
Apophis_ <ancientphoebus@gmail.com>
2021-09-24 17:53:34 +0530

    minor changes

--- lib/Widget/WidgetSecondScreen/SecondScreen.dart
+++ lib/Widget/WidgetSecondScreen/SecondScreen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatelessWidget {
-
-
+  static const routeName='/secondCategoryScreen';
+// final String title;
+// final String id;
+// SecondScreen({required this.title,required this.id});
   @override
   Widget build(BuildContext context) {
+    
+final recieveArgs=ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+final String? title=recieveArgs['title'];
+final String? id=recieveArgs['id'];
     return Scaffold(
-      appBar:AppBar(title:Text("SecondSreen")),
+      appBar:AppBar(title:Text(title!)),
       body:Text("Soon to be here stuff"),
       
     );
