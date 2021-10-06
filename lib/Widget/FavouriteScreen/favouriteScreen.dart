b54671adc441225b837b5a27a98686f1ef2db3b6
squidrye <ancientphoebus@gmail.com>
2021-10-06 18:39:20 +0530

    displays favourite meals

---
+++ lib/Widget/FavouriteScreen/favouriteScreen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class FavouriteScreen extends StatelessWidget{
+  Widget build(BuildContext ctx){
+    return Center(
+      child:Text("Your favourites will be shown here"),
+    );
+  }
+}
