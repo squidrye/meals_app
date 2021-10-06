c5b92b47580476ddd2fc0689302a89871dd4f8a7
squidrye <ancientphoebus@gmail.com>
2021-10-06 18:37:47 +0530

    side drawer for settings, filters

---
+++ lib/Widget/mainDrawer.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon,Function tapHandler){
+    return  ListTile(
+            leading: Icon(icon, size: 26),
+            title: Text(title,
+                style: TextStyle(
+                  // fontFamily: "RobotoCondensed",
+                  fontSize: 24,
+                  fontWeight: FontWeight.normal,
+                )),
+            onTap:(){tapHandler();});
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: Column(
+      children: [
+        Container(
+          color: Theme.of(context).backgroundColor,
+          height: 100,
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerLeft,
+          child: Text('Under dev',
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 30,
+                color: Theme.of(context).accentColor,
+              )),
+        ),
+        SizedBox(height: 20),
+       buildListTile("Meals",Icons.restaurant,(){}),
+       SizedBox(height: 20,),
+       buildListTile("Filters",Icons.settings,(){}),
+      ],
+    ));
+  }
+}
