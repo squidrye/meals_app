35649bbc5e65960e15ac21d4963d9221a5ce5be7
squidrye <ancientphoebus@gmail.com>
2021-10-10 20:16:46 +0530

    minor styling changes

--- lib/Widget/WidgetThirdScreen/mealDetails.dart
+++ lib/Widget/WidgetThirdScreen/mealDetails.dart
@@ -14,97 +14,98 @@ class MealDetails extends StatelessWidget {
     });
     Widget buildContainer(String title) {
       return Container(
-      
         padding: EdgeInsets.all(5),
         margin: EdgeInsets.all(5),
-        child: Text(title,style:Theme.of(ctx).textTheme.bodyText2),
+        child: Text(title, style: Theme.of(ctx).textTheme.bodyText2),
       );
     }
 
     Widget buildList(Widget child) {
       return Container(
           decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey),
-          borderRadius:BorderRadius.circular(10),
-        ),
-        margin: EdgeInsets.all(4),
-        height: 200,
-        width: double.infinity,
-        child: child
-      );
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: EdgeInsets.all(4),
+          height: 200,
+          width: double.infinity,
+          child: child);
     }
 
     return Scaffold(
-      appBar: AppBar(title: Text("${selectedMeal.title}")),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              decoration:BoxDecoration(
-                color:Colors.white,
-                border: Border.all(color:Colors.grey),
-                borderRadius:BorderRadius.circular(10),
-              ),
-              margin: EdgeInsets.all(4),
-              height: 300,
-              width: double.infinity,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(9),
+        appBar: AppBar(title: Text("${selectedMeal.title}")),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                elevation: 5,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(9),
-                    topRight: Radius.circular(9),
+                margin: EdgeInsets.all(4),
+                height: 300,
+                width: double.infinity,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(9),
                   ),
-                  child: Image.network(
-                    selectedMeal.imageUrl,
-                    fit: BoxFit.cover,
+                  elevation: 5,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(9),
+                      topRight: Radius.circular(9),
+                    ),
+                    child: Image.network(
+                      selectedMeal.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            buildContainer("Ingredients"),
-            buildList(Card(
-              color:Theme.of(ctx).backgroundColor,
-              child: ListView.builder(
-                itemBuilder: (BuildContext ctx, int index) {
-                  return Column(
-                    children: [
-                       Text(selectedMeal.ingredients[index]),
-                      Divider(),
-                    ],
-                  );
-                },
-                itemCount: selectedMeal.ingredients.length,
-              ),
-            )),
-            buildContainer("Recipie"),
-            buildList(Card(
-              color:Theme.of(ctx).backgroundColor,
-              child: ListView.builder(
-                itemBuilder: (BuildContext ctx, int index) {
-                  return Column(
-                    children: [
-                
-                         ListTile(
+              buildContainer("Ingredients"),
+              buildList(Card(
+                color: Theme.of(ctx).backgroundColor,
+                child: ListView.builder(
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return Column(
+                      children: [
+                        Text(selectedMeal.ingredients[index]),
+                        Divider(),
+                      ],
+                    );
+                  },
+                  itemCount: selectedMeal.ingredients.length,
+                ),
+              )),
+              buildContainer("Recipie"),
+              buildList(Card(
+                color: Theme.of(ctx).backgroundColor,
+                child: ListView.builder(
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return Column(
+                      children: [
+                        ListTile(
                             leading: CircleAvatar(
                               child: Text("${index + 1}"),
                             ),
                             title: Text(selectedMeal.steps[index])),
-                      
-                      Divider(),
-                    ],
-                  );
-                },
-                itemCount: selectedMeal.steps.length,
-              ),
-            ))
-          ],
+                        Divider(),
+                      ],
+                    );
+                  },
+                  itemCount: selectedMeal.steps.length,
+                ),
+              ))
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.delete),
+          onPressed:(){
+            Navigator.of(ctx).pop(idMeal);
+          }),
+        );
   }
 }
