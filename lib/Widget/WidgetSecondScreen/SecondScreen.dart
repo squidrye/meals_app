9fdc127865f3703bc82da3797787b484ca22af43
squidrye <ancientphoebus@gmail.com>
2021-10-10 20:15:21 +0530

    add didChangeDependency

--- lib/Widget/WidgetSecondScreen/SecondScreen.dart
+++ lib/Widget/WidgetSecondScreen/SecondScreen.dart
@@ -3,34 +3,57 @@ import 'package:meals_app/Widget/WidgetSecondScreen/singleMealRenderWidget.dart'
 import 'package:meals_app/models/meals.dart';
 import '../WidgetsFirstScreen/dummy_data.dart';
 
-class SecondScreen extends StatelessWidget {
+class SecondScreen extends StatefulWidget{
   static const routeName = '/secondCategoryScreen';
-// final String title;
-// final String id;
-// SecondScreen({required this.title,required this.id});
-  @override
-  Widget build(BuildContext context) {
-    final recieveArgs =
+  SecondScreenState createState(){
+    return SecondScreenState();
+  }
+
+}
+class SecondScreenState extends State<SecondScreen>{
+  var _loadInitialData=false;
+  String? title;
+  List<Meal>? filteredValue;
+void didChangeDependencies(){
+  if(!_loadInitialData){
+final recieveArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-    final String? title = recieveArgs['title'];
+     title = recieveArgs['title'];
     final String? id = recieveArgs['id'];
-    final filteredValue = DUMMY_MEALS.where((iterable) {
+    filteredValue = DUMMY_MEALS.where((iterable) {
       return iterable.categories.contains(id);
     }).toList();
+  }
+  _loadInitialData=true;
+  super.didChangeDependencies();
+}
+
+void removeItem(String id){
+ setState((){
+   filteredValue!.removeWhere((iterable){
+     return iterable.id==id;
+   });
+ });
+}
+  @override
+  Widget build(BuildContext context) {
+    
     
     return Scaffold(
       appBar: AppBar(title: Text(title!)),
       body: ListView.builder(
         itemBuilder: (BuildContext context,int index) {
           return SingleMealRender(
-              title: filteredValue[index].title,
-              imageUrl: filteredValue[index].imageUrl,
-              id: filteredValue[index].id,
-              affordability: filteredValue[index].affordability,
-              complexity: filteredValue[index].complexity,
-              duration: filteredValue[index].duration);
+              title: filteredValue![index].title,
+              imageUrl: filteredValue![index].imageUrl,
+              id: filteredValue![index].id,
+              affordability: filteredValue![index].affordability,
+              complexity: filteredValue![index].complexity,
+              duration: filteredValue![index].duration,
+              removeItem:removeItem,
+              );
         },
-        itemCount: filteredValue.length,
+        itemCount: filteredValue!.length,
       ),
     );
   }
