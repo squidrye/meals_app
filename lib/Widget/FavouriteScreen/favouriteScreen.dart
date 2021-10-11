c9fe02cb2d87917023f0b612686814b03b4815fb
squidrye <ancientphoebus@gmail.com>
2021-10-11 20:08:11 +0530

    add favscreen

--- lib/Widget/FavouriteScreen/favouriteScreen.dart
+++ lib/Widget/FavouriteScreen/favouriteScreen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meals.dart';
+import 'package:meals_app/Widget/WidgetSecondScreen/singleMealRenderWidget.dart';
 class FavouriteScreen extends StatelessWidget{
+  List<Meal> favouriteMeals;
+  FavouriteScreen(this.favouriteMeals);
   Widget build(BuildContext ctx){
-    return Center(
-      child:Text("Your favourites will be shown here"),
-    );
+    if(favouriteMeals.isEmpty){
+    return Center(child:Text("Your have no favourites yet"));      
+    }else {
+    return ListView.builder(
+        itemBuilder: (BuildContext context,int index) {
+          return SingleMealRender(
+              title: favouriteMeals[index].title,
+              imageUrl: favouriteMeals[index].imageUrl,
+              id: favouriteMeals[index].id,
+              affordability: favouriteMeals[index].affordability,
+              complexity: favouriteMeals[index].complexity,
+              duration: favouriteMeals[index].duration,
+              removeItem:(_){},
+              );
+        },
+        itemCount: favouriteMeals.length,
+      ); 
   }
+}
 }
